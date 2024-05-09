45873d6af60cf0a2c7cd0c0bc746532fba576acb
Annika Makela <amakela@c1r2p3.hive.fi>
2024-05-09 21:03:25 +0300

    fd handling fixed and '>>' added

--- src/fd_utils.c
+++ src/fd_utils.c
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:47:58 by amakela           #+#    #+#             */
-/*   Updated: 2024/05/04 17:10:53 by amakela          ###   ########.fr       */
+/*   Updated: 2024/05/09 21:02:26 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 // opens and closes correct fds for last child process
 static int	last_child(t_pipex *data, t_node *processes)
 {
-	data->ends[0] = dup(data->read_end);
-	if (data->read_end != -1)
-		close(data->read_end);
+	ft_printf(1, "last child\n");
+	dup2(data->read_end, data->ends[0]);
 	handle_redirs(processes, data);
 	return (0);
 }
@@ -25,6 +24,7 @@ static int	last_child(t_pipex *data, t_node *processes)
 // opens and closes correct fds for middle child processes
 static int	middle_child(t_pipex *data, t_node *processess)
 {
+	ft_printf(1, "middle child\n");
 	int	tmp;
 
 	if (pipe(data->ends) == -1)
@@ -44,9 +44,18 @@ static int	middle_child(t_pipex *data, t_node *processess)
 // opens and closes correct fds for first child process
 static int	first_child(t_pipex *data, t_node *processes)
 {
-	data->read_end = dup(data->ends[0]);
-	if (data->ends[0] != -1)
-		close(data->ends[0]);
+	ft_printf(1, "first child\n");
+	dup2(data->ends[0], data->read_end);
+	handle_redirs(processes, data);
+	return (0);
+}
+
+// opens and closes correct fds for parent process (only one builtin cmd)
+static int	parent(t_pipex *data, t_node *processes)
+{
+	ft_printf(1, "parent\n");
+	dup2(STDIN_FILENO, data->ends[0]);
+	dup2(STDOUT_FILENO, data->ends[1]);
 	handle_redirs(processes, data);
 	return (0);
 }
@@ -54,6 +63,8 @@ static int	first_child(t_pipex *data, t_node *processes)
 // opens and closes correct fds based on the process
 int	get_fds(t_pipex *data, t_node *processes)
 {
+	if (data->cmds == 1)
+		return (parent(data, processes));
 	if (data->count == 0)
 		return (first_child(data, processes));
 	if (data->count == data->cmds - 1)
