1d1a0d6697eb30f4f682b26487af8497c307d5a3
Annika Makela <amakela@c2r2p11.hive.fi>
2024-05-10 21:41:20 +0300

    printfs removed

--- src/fd_utils.c
+++ src/fd_utils.c
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:47:58 by amakela           #+#    #+#             */
-/*   Updated: 2024/05/09 21:02:26 by amakela          ###   ########.fr       */
+/*   Updated: 2024/05/10 14:30:25 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 // opens and closes correct fds for last child process
 static int	last_child(t_pipex *data, t_node *processes)
 {
-	ft_printf(1, "last child\n");
 	dup2(data->read_end, data->ends[0]);
 	handle_redirs(processes, data);
 	return (0);
@@ -24,7 +23,6 @@ static int	last_child(t_pipex *data, t_node *processes)
 // opens and closes correct fds for middle child processes
 static int	middle_child(t_pipex *data, t_node *processess)
 {
-	ft_printf(1, "middle child\n");
 	int	tmp;
 
 	if (pipe(data->ends) == -1)
@@ -44,7 +42,6 @@ static int	middle_child(t_pipex *data, t_node *processess)
 // opens and closes correct fds for first child process
 static int	first_child(t_pipex *data, t_node *processes)
 {
-	ft_printf(1, "first child\n");
 	dup2(data->ends[0], data->read_end);
 	handle_redirs(processes, data);
 	return (0);
@@ -53,7 +50,6 @@ static int	first_child(t_pipex *data, t_node *processes)
 // opens and closes correct fds for parent process (only one builtin cmd)
 static int	parent(t_pipex *data, t_node *processes)
 {
-	ft_printf(1, "parent\n");
 	dup2(STDIN_FILENO, data->ends[0]);
 	dup2(STDOUT_FILENO, data->ends[1]);
 	handle_redirs(processes, data);
