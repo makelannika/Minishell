27de64cc93ebe8fdef97fb38f9e50c35e750d14b
Annika Makela <140081469+makelannika@users.noreply.github.com>
2024-06-05 21:38:19 +0300

    expansion for redirs added

--- src/check_redirs.c
+++ src/check_redirs.c
@@ -12,11 +12,32 @@
 
 #include "../include/minishell.h"
 
+static int	expand_redir(t_pipex *data, char *redir)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (redir[i] == '<' || redir[i] == '>')
+		i++;
+	tmp = &redir[i];
+	expand_v2(data, &tmp);
+	if (!(*tmp))
+	{
+		ft_printf(2, "Mooshell: %s: No such file or directory\n", tmp);
+		data->execute = 0;
+		return (-1);
+	}
+	return (0);
+}
+
 static int	redir_out(char *file, t_pipex *data)
 {
 	int		i;
 
 	i = 1;
+	if (expand_redir(data, file) == -1)
+		return (set_exitcode(data, 1));
 	close(data->ends[1]);
 	if (file[1] == '>')
 		data->ends[1] = open(&file[2], O_CREAT | O_WRONLY | O_APPEND, 0644);
@@ -36,6 +57,8 @@ static int	redir_out(char *file, t_pipex *data)
 
 static int	redir_in(char *file, t_pipex *data)
 {
+	if (expand_redir(data, file) == -1)
+		return (set_exitcode(data, 1));
 	close(data->ends[0]);
 	data->ends[0] = open(&file[1], O_RDONLY);
 	if (data->ends[0] < 0)
@@ -77,12 +100,12 @@ int	handle_redirs(t_node *process, t_pipex *data)
 		}
 		else if (ft_strncmp(process->redirs[i], "<", 1) == 0)
 		{
-			if (redir_in(process->redirs[i], data) == 1)
+			if (redir_in(process->redirs[i], data) == -1)
 				return (data->exitcode);
 		}
 		else if (ft_strncmp(process->redirs[i], ">", 1) == 0)
 		{
-			if (redir_out(process->redirs[i], data) == 1)
+			if (redir_out(process->redirs[i], data) == -1)
 				return (data->exitcode);
 		}
 		i++;
